10093dcab37ebc99dc259fcdd4fe483cceca54ca
Ahmad Abdullah <business.ahmadabdullah@gmail.com>
2024-05-12 22:44:40 +0500

    remove backend

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-# MAC
-conn = pyodbc.connect(
-       'DRIVER=/opt/homebrew/lib/libmsodbcsql.17.dylib' + 
-       ';SERVER=' + 'localhost,1433' + ';UID=' + 'sa' + 
-       ';PWD=' + 'dockerStrongPwd123' +
-       ';database=ESPNCricInfo')
